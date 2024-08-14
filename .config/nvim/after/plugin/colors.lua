dcfcac02f772ff15fd2a03270dd8a8eccddd3d82
Ivan Paraskun <ig.paraskun@gmail.com>
2024-08-14 07:28:23 +0700

    changed styles for comments.

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -22,7 +22,13 @@ function ColorMyPencils(color)
 	vim.cmd.colorscheme(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalSB", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalNCFloat", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalSBFloat", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+  vim.api.nvim_set_hl(0, "Comment", { italic = true })
 end
 
 ColorMyPencils()
