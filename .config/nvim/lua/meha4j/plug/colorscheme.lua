6ef930f1023dd71e0473613394152b537357ab0b
Ivan Paraskun <ig.paraskun@gmail.com>
2024-10-06 21:22:26 +0700

    upd.

--- .config/nvim/lua/meha4j/plug/colorscheme.lua
+++ .config/nvim/lua/meha4j/plug/colorscheme.lua
@@ -5,6 +5,15 @@ return {
   priority = 1000,
 
   config = function()
+    require("tokyonight").setup({
+      transparent = true,
+      styles = {
+        comments = { italic = true },
+        keywords = { italic = false },
+        floats = { transparent = true }
+      },
+    })
+
     vim.cmd("colorscheme tokyonight")
   end
 }
